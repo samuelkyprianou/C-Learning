93626b2a209b8048e6b1ab8227a3ae5dc3422c04
samuelkyprianou <samuelkyprianou@gmail.com>
2020-06-30 14:58:33 +0100

    variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didnt like being " + characterAge);
+        }
+    }
+}
