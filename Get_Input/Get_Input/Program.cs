df9b12d15b69012b741dfb751254e69a7953b67f
samuelkyprianou <samuelkyprianou@gmail.com>
2020-07-02 14:25:38 +0100

    user input

---
+++ Get_Input/Get_Input/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Get_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Your Age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You Are " + age + " Years Old");
+        }
+    }
+}
---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 8);
+            Console.WriteLine(5 - 8);
+            Console.WriteLine(5 * 8);
+            Console.WriteLine(10 / 5);
+            Console.WriteLine(5 % 2);
+            Console.WriteLine((4 + 2) * 2);
+            Console.WriteLine((4 + 2) * 2);
+            Console.WriteLine(5 / 2.0);
+
+            int num = 6;
+            num++;
+            Console.WriteLine(num);
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 3));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(4, 90));
+            Console.WriteLine(Math.Round(4.6));
+
+
+        }
+    }
+}
