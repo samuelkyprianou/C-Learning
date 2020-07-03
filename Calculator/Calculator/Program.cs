e33fc1059bec73f266209ac08540d842a3245666
samuelkyprianou <samuelkyprianou@gmail.com>
2020-07-03 14:50:55 +0100

    calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A Number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Another Number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
