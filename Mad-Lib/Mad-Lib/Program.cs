76954ae5a85dfc0672f56d22c7607158385cf9bf
samuelkyprianou <samuelkyprianou@gmail.com>
2020-07-13 22:42:20 +0100

    mad lib

---
+++ Mad-Lib/Mad-Lib/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mad_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string colour, pluralNoun, celebrity;
+
+            Console.Write("Enter a colour: ");
+            colour = Console.ReadLine();
+            Console.Write("Enter a pluralNoun: ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + colour);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+        }
+    }
+}
